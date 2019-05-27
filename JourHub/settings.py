aa3bf70902ee3cf19fcb84852218782439914680
feynman1999 <chenyx.cs@gmail.com>
2019-05-27 23:00:34 +0800

    完善页脚

--- JourHub/settings.py
+++ JourHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r7ijr9d^#do4^e^#oce!7)zmqgk75-#)6*nn(3)f37a*(x0c2j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
 ]
